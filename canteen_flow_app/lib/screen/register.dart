87e367a9601155e79909833faa7086872ef17af5
Vimukthi Jayasanka <vimukthi200020@gmail.com>
2024-01-07 18:01:06 +0530

    OGG

--- canteen_flow_app/lib/screen/register.dart
+++ canteen_flow_app/lib/screen/register.dart
@@ -1,108 +1,180 @@
+import 'package:canteen_flow_app/screen/home.dart';
+import 'package:canteen_flow_app/screen/loging.dart';
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Create Account',
-                  style: TextStyle(
-                    color: Color(0xFFF5B358),
-                    fontSize: 30,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w700,
-                  ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: 400,
+                height: 750,
+                padding: const EdgeInsets.only(
+                  left: 36,
+                  right: 35,
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'Create an account to give feedback to improve canteens.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF2A2D37),
-                    fontSize: 14,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Confirm Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 20),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(color: Colors.white),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        // Implement user button functionality
-                      },
-                      child: Text('User'),
+                    const Text(
+                      'Create Account',
+                      style: TextStyle(
+                        color: Color(0xFFF5B358),
+                        fontSize: 40,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
                     ),
-                    OutlinedButton(
-                      onPressed: () {
-                        // Implement admin button functionality
-                      },
-                      child: Text('Admin'),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Create an account so you can Give feedbacks to improving canteens.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF2A2D37),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                      ),
+                    ),
+                    const SizedBox(height: 74),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                             color: Color(0xFFF1F4FF),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Enter your email',
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF1F4FF),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: const TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your password',
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF1F4FF),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: const TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Confirm your password',
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        const SizedBox(height: 70),
+                        Container(
+                          width: 320,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomeScreen(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: const Color.fromRGBO(245, 179, 88, 1),
+                            ),
+                            child: const Text(
+                              'Sign Up',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          width: 357,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Already have an account',
+                                  style: TextStyle(
+                                    color: Color(0xFF2A2D37),
+                                    fontSize: 16,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement sign-in functionality
-                  },
-                  child: Text('Sign in'),
-                ),
-                SizedBox(height: 20),
-                OutlinedButton(
-                  onPressed: () {
-                    // Implement sign-up functionality
-                  },
-                  child: Text('Create new account'),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    // Navigate to login screen
-                  },
-                  child: Text('Already have an account'),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+class HomeScreen {
+  const HomeScreen();
 }
