0d858f3cd3738b9444c41866d2fc77caf17e47d4
Vimukthi Jayasanka <vimukthi200020@gmail.com>
2024-01-09 17:43:19 +0530

    connect DB

--- canteen_flow_app/lib/screen/loging.dart
+++ canteen_flow_app/lib/screen/loging.dart
@@ -1,13 +1,29 @@
+<<<<<<< Updated upstream
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+=======
+import 'package:canteen_flow_app/screen/home.dart';
+import 'package:canteen_flow_app/screen/register.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
+<<<<<<< Updated upstream
             child: Column(
           children: [
             Container(
@@ -145,10 +161,104 @@ class LoginScreen extends StatelessWidget {
                                   ),
                                 ],
                               ),
+=======
+          child: Column(
+            children: [
+              Container(
+                width: 400,
+                height: 750,
+                padding: const EdgeInsets.only(
+                  left: 36,
+                  right: 35,
+                ),
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(color: Colors.white),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Login here',
+                      style: TextStyle(
+                        color: Color(0xFFF5B358),
+                        fontSize: 40,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Welcome back you’ve been missed!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF2A2D37),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                      ),
+                    ),
+                    const SizedBox(height: 74),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF1F4FF),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Enter your email',
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF1F4FF),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: const TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your password',
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Forgot your password?',
+                                style: TextStyle(
+                                  color: Color(0xFF2A2D37),
+                                  fontSize: 15,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+>>>>>>> Stashed changes
                             ),
                           ],
                         ),
                         const SizedBox(height: 30),
+<<<<<<< Updated upstream
                         const Text(
                           'Forgot your password?',
                           textAlign: TextAlign.center,
@@ -158,10 +268,33 @@ class LoginScreen extends StatelessWidget {
                             fontFamily: 'Poppins',
                             fontWeight: FontWeight.w600,
                             height: 0,
+=======
+                        Container(
+                          width: 320,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomeScreen(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: const Color.fromRGBO(245, 179, 88, 1),
+                            ),
+                            child: const Text(
+                              'Sign In',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+>>>>>>> Stashed changes
                           ),
                         ),
                         const SizedBox(height: 30),
                         Container(
+<<<<<<< Updated upstream
                           child: Column(
                             mainAxisSize: MainAxisSize.min,
                             mainAxisAlignment: MainAxisAlignment.start,
@@ -232,6 +365,35 @@ class LoginScreen extends StatelessWidget {
                                       ),
                                     ),
                                   ],
+=======
+                          width: 357,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () { Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const RegisterScreen(),
+                                ),
+                                 ); },
+                                child: const Text(
+                                  'Create new account',
+                                  style: TextStyle(
+                                    color: Color(0xFF2A2D37),
+                                    fontSize: 16,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w600,
+                                  ),
+>>>>>>> Stashed changes
                                 ),
                               ),
                             ],
@@ -239,12 +401,21 @@ class LoginScreen extends StatelessWidget {
                         ),
                       ],
                     ),
+<<<<<<< Updated upstream
                   ),
                 ],
               ),
             ),
           ],
         )),
+=======
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+>>>>>>> Stashed changes
       ),
     );
   }
--- canteen_flow_app/lib/screen/register.dart
+++ canteen_flow_app/lib/screen/register.dart
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 import 'package:canteen_flow_app/screen/home.dart';
 import 'package:canteen_flow_app/screen/loging.dart';
 import 'package:flutter/material.dart';
+=======
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+>>>>>>> Stashed changes
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -10,9 +17,59 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  Future<void> register() async {
+    await dotenv.load();
+
+    String backend = dotenv.env['BACKEND'] as String;
+
+    final String apiUrl =
+        '$backend/registration'; // Replace with your actual backend URL
+
+    // Validate password and confirm password
+    if (passwordController.text != confirmPasswordController.text) {
+      // Passwords do not match
+      print('Passwords do not match');
+      return;
+    } else {
+      print("Passwords match");
+      print(emailController.text);
+      print(passwordController.text);
+    }
+
+    var data = {
+      'email': emailController.text,
+      'password': passwordController.text,
+    };
+
+    try {
+      Dio dio = Dio();
+
+      // Increase the timeout (adjust as needed)
+      dio.options.connectTimeout = const Duration(seconds: 5);
+
+      final response = await dio.post(
+        apiUrl,
+        data: {
+          'email': emailController.text,
+          'password': passwordController.text
+        },
+      );
+      // Handle response as needed
+      print(response.data); // You may want to log or process the response
+    } catch (e) {
+      print(e);
+      // Handle error as needed
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -166,15 +223,162 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                   ],
                 ),
+=======
+      appBar: AppBar(
+        title: const Text('Create Account'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Create Account',
+              style: TextStyle(
+                color: Color(0xFFF5B358),
+                fontSize: 40,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w700,
+                height: 0,
+>>>>>>> Stashed changes
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Create an account so you can Give feedbacks to improving canteens.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFF2A2D37),
+                fontSize: 15,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+                height: 0,
+              ),
+            ),
+            const SizedBox(height: 74),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF1F4FF),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your email',
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF1F4FF),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your password',
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF1F4FF),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: confirmPasswordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Confirm your password',
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 70),
+                Container(
+                  width: 320,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: register,
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromRGBO(245, 179, 88, 1),
+                    ),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  width: 357,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Already have an account',
+                          style: TextStyle(
+                            color: Color(0xFF2A2D37),
+                            fontSize: 16,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+<<<<<<< Updated upstream
 class HomeScreen {
   const HomeScreen();
+=======
+void main() {
+  runApp(
+    MaterialApp(
+      home: RegisterScreen(),
+    ),
+  );
+>>>>>>> Stashed changes
 }
